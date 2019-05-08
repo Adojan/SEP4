4b97c60c76b29ba1f404fb73ccff4006625ee6be
AlexandruVieru <33315908+AlexandruVieru@users.noreply.github.com>
2019-05-08 23:13:22 +0200

    cf

--- SEP4xyz-Android code Miercuri 2019/app/src/main/java/com/example/sep4xyz/MainAdminPage.java
+++ SEP4xyz-Android code Miercuri 2019/app/src/main/java/com/example/sep4xyz/MainAdminPage.java
@@ -7,38 +7,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainAdminPage extends AppCompatActivity {
+public class MainAdminPage extends AppCompatActivity implements View.OnClickListener {
 
-    Button createUser;
-    Button viewData;
-    
+    private Button createUser;
+    private Button viewData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_admin_page);
-        createUser = (Button) findViewById(R.id.createUser);
-        viewData = (Button) findViewById(R.id.viewData);
-
+        createUser = findViewById(R.id.createUser);
+        viewData = findViewById(R.id.viewData);
+        createUser.setOnClickListener(this);
+        viewData.setOnClickListener(this);
 
     }
 
-    // create user  intent
-    public void createUser(View view) {
-        Intent createUser = new Intent(MainAdminPage.this, MainCreateUsers.class);
-        startActivity(createUser);
-    }
-
-    // view data intent
-    public void viewData(View view) {
-        Intent viewData = new Intent(MainAdminPage.this, SelectBlock.class);
-        startActivity(viewData);
+    @Override
+    public void onClick(View view) {
+        if (view == createUser) {
+            Intent createUser = new Intent(MainAdminPage.this, MainCreateUsers.class);
+            startActivity(createUser);
+        }
+        if (view == viewData) {
+            Intent viewData = new Intent(MainAdminPage.this, SelectBlock.class);
+            startActivity(viewData);
+        }
     }
 
     // floating button create user intent
     public void floatCreateUser(View view) {
         Intent createUser = new Intent(MainAdminPage.this, MainCreateUsers.class);
         startActivity(createUser);
+
+
     }
 
 }
