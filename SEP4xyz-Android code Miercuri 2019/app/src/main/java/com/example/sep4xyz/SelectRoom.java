8cce6d45f055867a45ddc8ba32a066b7ec351b34
AlexandruVieru <33315908+AlexandruVieru@users.noreply.github.com>
2019-05-09 15:35:13 +0200

    d

--- SEP4xyz-Android code Miercuri 2019/app/src/main/java/com/example/sep4xyz/SelectRoom.java
+++ SEP4xyz-Android code Miercuri 2019/app/src/main/java/com/example/sep4xyz/SelectRoom.java
@@ -1,13 +1,23 @@
 package com.example.sep4xyz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SelectRoom extends AppCompatActivity {
+
+    TextView F307;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_room);
+        F307=findViewById(R.id.F307);
+    }
+    public void openroom (View view){
+        Intent openRoom= new Intent(SelectRoom.this,ViewData.class);
+        startActivity(openRoom);
     }
 }
