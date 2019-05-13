f3a804dc9a0276e3a71b6c7332f0a2503e835510
RaresBlea <bunea.rares@yahoo.com>
2019-05-13 15:26:52 +0200

    android with arhitecture

---
+++ sep android final/app/src/main/java/com/example/SEP_XYZ/viewmodels/LoginViewModel.java
@@ -0,0 +1,50 @@
+package com.example.SEP_XYZ.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import com.example.SEP_XYZ.models.Administrator;
+import com.example.SEP_XYZ.models.FireBaseModel;
+
+
+public class LoginViewModel extends ViewModel {
+
+    private FireBaseModel mFireBaseModel;
+    private Administrator administrator;
+
+    public void init() {
+        if (mFireBaseModel != null) return;
+        mFireBaseModel = new FireBaseModel();
+        if(administrator!=null)return;
+        administrator=new Administrator();
+    }
+
+    public FireBaseModel getmFireBaseModel() {
+        return mFireBaseModel;
+    }
+    public String geStringFromTextView(TextView textView)
+    {
+        String s = textView.getText().toString();
+        return s;
+    }
+    public boolean checkEmptyTextField(TextView textView)
+    {
+        return TextUtils.isEmpty(textView.getText().toString());
+    }
+
+    public Administrator getAdministrator()
+    {
+        return administrator;
+    }
+
+    public boolean checkIfAdministrator(TextView email, TextView password)
+    {
+        String emailAdmin=geStringFromTextView(email);
+        String passwordAdmin=geStringFromTextView(password);
+        return  (administrator.getEmail().equals(emailAdmin)&& administrator.getPassword().equals(passwordAdmin));
+
+    }
+
+
+}
---
+++ sep android final/app/src/main/java/com/example/SEP_XYZ/views/SelectRoomActivity.java
@@ -0,0 +1,15 @@
+package com.example.SEP_XYZ.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.SEP_XYZ.R;
+
+public class SelectRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_block);
+    }
+}
