acc4f2f5f32de83f385ae3a69bed830dd7e8c0ae
RaresBlea <bunea.rares@yahoo.com>
2019-05-13 17:07:31 +0200

    code

--- SEP_AND/app/src/main/java/com/example/SEP_XYZ/models/RoomList.java
+++ SEP_AND/app/src/main/java/com/example/SEP_XYZ/models/RoomList.java
@@ -1,4 +1,39 @@
 package com.example.SEP_XYZ.models;
 
+import java.util.ArrayList;
+
 public class RoomList {
+
+    private ArrayList<Room> roomList;
+
+    public RoomList() {
+
+    }
+
+    public ArrayList<Room> getRoomList() {
+        return roomList;
+    }
+
+    public void setRoomList(ArrayList<Room> roomList) {
+        this.roomList = roomList;
+    }
+    public int getSize()
+    {
+        return roomList.size();
+    }
+    public Room getRoomById(String id)
+    {
+        for (int i=0;i<roomList.size();i++)
+        {
+            if(roomList.get(i).getRoomId().equals(id))
+                return roomList.get(i);
+
+        }
+        return null;
+    }
+
+    public Room getRoomAtIndex(int index)
+    {
+        return roomList.get(index);
+    }
 }
